cdb300fb33505478458b483de75fe067ceced86d
Sebastián Vicencio <sivicencio@gmail.com>
2019-10-17 07:16:42 +0000

    feat(Book): add authors scaffold and base for Activity 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # namespace :admin do
+    # resources :books
+    # resources :authors
+  # end
+
   resources :books, only: [:index, :show]
 
   root to: 'books#index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_09_150107) do
+ActiveRecord::Schema.define(version: 2019_10_16_200638) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
